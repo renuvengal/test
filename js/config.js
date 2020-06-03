7e2df4bdb3af0923a381c66ddee22e35271489ee
EC2 Default User <ec2-user@ip-172-31-55-6.ec2.internal>
2020-06-03 18:38:46 +0000

    configure cognito

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_jG7Ap7t7l', // e.g.us-east-1_jG7Ap7t7l
+        userPoolClientId: '4ulbt833k0acset0iiq8gp99v8', // e.g. 4ulbt833k0acset0iiq8gp99v8
+        region: 'us-east-1' // e.g. us-east-1
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-1.amazonaws.com/prod',
+    }
+};
